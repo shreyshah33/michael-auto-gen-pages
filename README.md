f503a1ddedad491d8c128afa4e02de66fba3766c
shreyshah33 <shreyshah33@gmail.com>
2021-12-23 21:19:00 -0600

    Added index and js pages

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # michael-auto-gen-pages
- 
---
+++ data.js
@@ -0,0 +1,14 @@
+data = {
+	1: {
+		title: "howdy1",
+		description: "howdy1 description",
+	},
+	2: {
+		title: "howdy2",
+		description: "howdy2 description",
+	},
+	3: {
+		title: "howdy3",
+		description: "howdy3 description",
+	},
+};
